6ab0a4c7ef8a59e35f1435e1ed9332ad474ad610
shivnandk <113520167+shivnandk@users.noreply.github.com>
2024-07-11 16:56:44 +0000

    datatypes padha

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accoutId=14453
+
+let accountEmail="shivnand@gmail.com"
+var accountPassword="shivnand123"
+accountCity="Indore"
+let accountState
+
+// accoutId=2
+//constanst is in mutablel
+
+accountEmail="shiv@gmail.com"
+accountPassword="shiv1234"
+accountCity="Bhopal"
+// console.log(accoutId)   
+console.table([accoutId,accountEmail,accountPassword,accountCity,accountState])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";//treat all js code as new version
+// alert("Hello, World!"); we are using node js not browser
+console.log("shivnand");
+console.log("khatri");
+
+let name="shivnand";
+let age=20;
+let isLoggedIn=false;
+
+//number 2 to the power 53-1
+//bigit yha bht bada number hota hai
+//boolean
+//string ""
+//null =>standalone value
+//undefined
+//symbol apn me use karenge
+console.log(typeof undefined);
