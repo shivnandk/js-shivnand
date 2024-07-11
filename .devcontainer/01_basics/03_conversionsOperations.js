262f889a028a90adfb0bea561a0f925465de9b22
shivnandk <113520167+shivnandk@users.noreply.github.com>
2024-07-11 17:06:19 +0000

    conversions of data types

---
+++ .devcontainer/01_basics/03_conversionsOperations.js
@@ -0,0 +1,23 @@
+let score="100"
+let score2="100s"
+
+console.log(typeof score)
+//converting string to number
+let no=Number(score);
+let no2=Number(score2);
+console.log(typeof no)  
+console.log(typeof no2)
+console.log(no2)  
+
+//"33"=>33
+//"33abc"=>NaN
+
+let isLoggedIn=1;
+
+//1 true
+//0 false
+//""=>false
+//"shivnand"=>true
+
+let somenumber=55;
+console.log
